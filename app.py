4d10b1d40449c7245c72f44f5d368f61b3f884f5
afnan2007 <93235339+afnan2007@users.noreply.github.com>
2024-05-15 18:32:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import sklearn
 from sklearn.linear_model import RidgeClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import SequentialFeatureSelector
