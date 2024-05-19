e145eafa0bb4779a75acfdb0954954d30998f9ff
afnan2007 <93235339+afnan2007@users.noreply.github.com>
2024-05-19 19:11:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 import pandas as pd
 import datetime 
-import sklearn
-from sklearn.linear_model import RidgeClassifier
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.feature_selection import SequentialFeatureSelector
-from sklearn.model_selection import TimeSeriesSplit
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+
 
 st.set_page_config(page_title="NBA Matchup Predictor", layout="wide")
 
@@ -37,8 +35,8 @@ st.markdown('<div class="title"><h1>NBA Match Predictor Using ML</h1></div>', un
 team1, middle, team2 = st.columns([2,2,2])
 
 
-teams = ['ATL', 'BOS', 'BKN', 'CHA', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 
-        'MEM', 'MIA', 'MIL', 'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHX', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
+teams = ['ATL', 'BOS', 'BRK', 'CHO', 'CHI', 'CLE', 'DAL', 'DEN', 'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 
+        'MEM', 'MIA', 'MIL', 'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO', 'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
 
 with team1: 
     st.markdown('<div class="title"><h1>Team 1</h1></div>', unsafe_allow_html=True)
@@ -46,7 +44,7 @@ with team1:
 
 with middle: 
      st.markdown('<div class="title"><h1>Match Date</h1></div>', unsafe_allow_html=True)
-     a, mid, three = st.columns([1,1,1])
+     a, mid, c = st.columns([1,1,1])
      with mid: 
         dat = st.date_input("Match Date:", value=datetime.date(2024, 6, 17))
      
@@ -54,103 +52,102 @@ with team2:
      st.markdown('<div class="title"><h1>Team 2</h1></div>', unsafe_allow_html=True)
      two = st.selectbox(label = "NBA Team #2:", options=teams)
 
-data = pd.read_csv('nba_games.csv')
+@st.cache_data
+def get_data():
+    data = pd.read_csv('nba_games.csv', index_col = 0)
+    data = data.sort_values("date").reset_index(drop=True)
+    del data["mp.1"]
+    del data["mp_opp.1"]    
+    del data["index_opp"]
+    return data
 
+data = get_data()
 st.write(data)
 
-one, mid, two = st.columns([1,1,1])
-with mid: 
-    one, mid, two = st.columns([1,1,1])
-    with mid:
+data["date"] = pd.to_datetime(data["date"])
+
+data["team_opp_code"] = data["team_opp"].astype("category").cat.codes
+
+data["target"] = (data["won"] == True).astype("int")
+
+
+rf = RandomForestClassifier(n_estimators=50, min_samples_split=10, random_state=1)
+
+train = data[data["date"] < '2018-01-01']
+test = data[data["date"] > '2018-01-01']
+
+predictors = ["home", "team_opp_code", "fg%", "3p%", "ast", "stl", "blk", "ts%", "fg%_opp", "3p%_opp", "ast_opp", "stl_opp", "blk_opp", "ts%_opp" ]
+
+rf.fit(train[predictors], train["target"])
+preds = rf.predict(test[predictors])
+
+combined = pd.DataFrame(dict(actual=test["target"], prediction=preds))
+
+grouped_data = data.groupby(["team", "team_opp"])
+def rolling_averages(group, cols, new_cols):
+    group = group.sort_values("date")
+    rolling_stats = group[cols].rolling(10, closed='left').mean()
+    group[new_cols] = rolling_stats
+    group = group.dropna(subset=new_cols)
+    return group
+
+cols = ['pts', 'orb', 'drb', 'ast', 'stl', 'blk', 'pts_opp', 'orb_opp', 'drb_opp', 'ast_opp', 'stl_opp', 'blk_opp']
+new_cols = [f"{c}_rolling" for c in cols]
+
+data_rolling = data.groupby(["team", "team_opp"]).apply(lambda x: rolling_averages(x, cols, new_cols))
+data_rolling.index = range(data_rolling.shape[0])
+
+def make_predictions(data, predictors):
+    train = data[data["date"] < '2018-1-01']
+    test = data[data["date"] > '2018-1-01']
+    rf.fit(train[predictors], train["target"])
+    preds = rf.predict(test[predictors])
+    combined = pd.DataFrame(dict(actual=test["target"], prediction=preds), index=test.index)
+    precision = precision_score(test["target"], preds)
+    return combined, precision
+
+combined, precision = make_predictions(data_rolling, predictors + new_cols)
+
+combined = combined.merge( data_rolling[["date", "team", "team_opp", "won"]], left_index=True, right_index=True)
+
+merged = combined.merge(combined, left_on=["date", "team"], right_on=["date", "team_opp"])
+
+merged = merged.sort_values("date").reset_index(drop=True)
+
+d, middy, e = st.columns([1,1,1])
+with middy: 
+    f, midd, g = st.columns([1,1,1])
+    with midd:
         but = st.button("Generate Outcome")
 
-data = data.sort_values("date")
-data = data.reset_index(drop=True)
-del data["mp.1"]
-del data["mp_opp.1"]
-del data["index_opp"]
+if but: 
+    matchup_exists = ((data["team"] == one) & (data["team_opp"] == two) | (data["team"] == two) & (data["team_opp"] == one)) & (data["date"] == pd.to_datetime(dat))
 
-def add_target(team):
-    team["target"] = team["won"].shift(-1)
-    return team  
+    if matchup_exists.any():
 
-data = data.groupby("team", group_keys=False).apply(add_target)
-data["target"][pd.isnull(data["target"])] = 2
-data["target"] = data["target"].astype(int, errors="ignore")
+        team1_str = str(one)
+        team2_str = str(two)
+        specific_matchup_index = merged[(merged["team_x"] == team1_str) & (merged["team_opp_x"] == team2_str) & (merged["date"] == pd.to_datetime(dat))].index[0]
+        specific_prediction = merged.loc[specific_matchup_index, "prediction_x"]
 
-nulls = pd.isnull(data)
-nulls = nulls.sum()
-nulls = nulls[nulls > 0]
-valid_columns = data.columns[~data.columns.isin(nulls.index)]
-data = data[valid_columns].copy()
-
-ridge = RidgeClassifier(alpha=1)
-split = TimeSeriesSplit(n_splits=3)
-sfs = SequentialFeatureSelector(ridge, n_features_to_select=30, direction="forward", cv=split)
-
-removed_columns = ["season", "date", "won", "target", "team", "team_opp"]
-selected_columns = data.columns[~data.columns.isin(removed_columns)]
-
-scaler = MinMaxScaler()
-data[selected_columns] = scaler.fit_transform(data[selected_columns])
-sfs.fit(data[selected_columns], data["target"])
-predictors = list(selected_columns[sfs.get_support()])
-
-data_rolling = data[list(selected_columns) + ["won", "team", "season"]]
-
-def team_averages(team):
-    numeric_cols = team.select_dtypes(include=['number']).columns
-    rolling = team[numeric_cols].rolling(10).mean()
-    return rolling
-
-data_rolling = data_rolling.groupby(["team", "season"], group_keys=False).apply(team_averages)
-rolling_cols = [f"{col}_10" for col in data_rolling.columns]
-data_rolling.columns = rolling_cols
-
-data = pd.concat([data, data_rolling], axis=1)
-data = data.dropna()
-
-def shift_col(team, col_name):
-    next_col = team[col_name].shift(-1)
-    return next_col
-
-def add_col(data, col_name):
-    return data.groupby("team", group_keys=False).apply(lambda x: shift_col(x, col_name))
-
-data["home_next"] = add_col(data, "home")
-data["team_opp_next"] = add_col(data, "team_opp")
-data["date_next"] = add_col(data, "date")
-
-data = data.copy()
-
-full = data.merge(data[rolling_cols + ["team_opp_next", "date_next", "team"]], 
-                  left_on=["team", "date_next"], 
-                  right_on=["team_opp_next", "date_next"])
-
-removed_columns_full = list(full.select_dtypes(include=['object']).columns) + removed_columns
-selected_columns_full = full.columns[~full.columns.isin(removed_columns_full)]
-
-
-full = full.dropna(subset=selected_columns_full)
-full = full.dropna(subset=["target"])
-
-sfs.fit(full[selected_columns_full], full["target"])
-
-selected_features_mask = [col in predictors for col in full.columns]
-selected_columns = full.columns[selected_features_mask]
-
-if but:
-    match_data = full[(full["team"] == one) & (full["team_opp_next"] == two) & (full["date_next"] == dat)]
-    
-    if match_data.empty:
-        st.markdown('<div class="title"><h1>No data available for this matchup and date.</h1></div>', unsafe_allow_html=True)
-    else:
-        predictors = list(selected_columns)
-        prediction = ridge.predict(match_data[predictors])[0]
-        if prediction == 1:
-            st.markdown(f'<div class="title"><h1>{one} won the game against {two} on {dat}.</h1></div>', unsafe_allow_html=True)
+        if specific_prediction == 1:
+            st.markdown(f'<div class="title"><h1>Based on the ML algorithm, {one} won the matchup against {two} on {dat}.</h1></div>', unsafe_allow_html=True)
         else:
-            st.markdown(f'<div class="title"><h1>{two} won the game against {one} on {dat}.</h1></div>', unsafe_allow_html=True)
+            st.markdown(f'<div class="title"><h1>Based on the ML algorithm, {two} won the matchup against {one} on {dat}.</h1></div>', unsafe_allow_html=True)
+    else:
+        st.markdown(f'<div class="title"><h1>No matchup occured between {one} and {two} on {dat}.</h1></div>', unsafe_allow_html=True)
+
+h, middl, i = st.columns([1,1,1])
+
+with middl:
+    st.write(merged)
+
+
+    
+
+    
+
+
 
 
 
